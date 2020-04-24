53f567d5a522c217af6372128736be4bc6b1a19b
vejei <chengsnem@163.com>
2020-04-24 21:52:07 +0800

    MainActivity

--- app/src/main/java/com/example/contentprovidersamples/MainActivity.kt
+++ app/src/main/java/com/example/contentprovidersamples/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.contentprovidersamples
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, MainFragment())
+                .commit()
+        }
+    }
+
+    fun showDirectoryContent(directoryUri: Uri) {
+        Log.d(TAG, "directory uri: $directoryUri")
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, DirectoryListFragment.newInstance(directoryUri), directoryUri.toString())
+            .addToBackStack(directoryUri.toString())
+            .commit()
+    }
+
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
     }
 }
